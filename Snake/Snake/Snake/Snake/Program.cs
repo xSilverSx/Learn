f3a25f1683fad4032a5a786a361fcd8b6aadc6e3
Владимир <e-hoooo@yandex.ru>
2016-08-21 10:37:51 +0300

    Revert "Первые наброски"
    This reverts commit 18078e4349ed563986b3c441aff0c5223443e0e1.

--- Snake/Snake/Snake/Snake/Program.cs
+++ Snake/Snake/Snake/Snake/Program.cs
@@ -10,18 +10,8 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1=5;
-            int y1 = 6;
-            char sym1= '*';
-
-            Draw(x1, y1, sym1);
+            Console.Write("Hello World!");
             Console.ReadLine();
-        }
-
-        static void Draw(int  x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
